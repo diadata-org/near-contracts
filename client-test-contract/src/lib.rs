e69b6d6ae7dfa90662cc5263bd816f695d8c40d3
Lautaro <lautarocutri@gmail.com>
2020-12-17 17:08:32 -0300

    added data structs for callback and contract initialization

--- client-test-contract/src/lib.rs
+++ client-test-contract/src/lib.rs
@@ -1,6 +1,6 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::serde::{Serialize};
-use near_sdk::{env, near_bindgen, Balance, Gas, AccountId};
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{env, near_bindgen, Balance, Gas};
 use std::fmt::Write;
 
 #[global_allocator]
@@ -14,7 +14,7 @@ const DIA_GATEWAY_ACCOUNT_ID: &str = "test.dia-sc.testnet";
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct ClientTestContract {
     pub contract_id: String,
-    pub callbackResponse: Response
+    pub callback_response: Response
 }
 
 #[derive(Serialize)]
@@ -26,55 +26,78 @@ pub struct DiaGatewayRequestArgs {
     callback: String
 }
 
+#[derive(Deserialize, Serialize, BorshDeserialize, BorshSerialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
+pub enum ResponseData{
+    Quote(QuoteData),
+    None
+}
+
 #[derive(Serialize, BorshDeserialize, BorshSerialize, Clone)]
 #[serde(crate = "near_sdk::serde")]
 pub struct Response{
+    err: String,
+    data: ResponseData,
+}
+
+#[derive(Deserialize, Serialize, BorshDeserialize, BorshSerialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
+pub struct QuoteData {
     symbol: String,
     name: String,
     price: f64,
     price_yesterday: f64,
-    volume_yesterday_USD: f64,
+    volume_yesterday_usd: f64,
     source: String,
     time: String,
-    itin; String,
+    itin: String,
     signer_account_id: String
-}
-
-impl Default for Response{
-    fn default()-> Self {
-        return Self {
-            symbol: String::new(),
-            name: String::new(),
-            price: 0,
-            price_yesterday: 0,
-            volume_yesterday_USD: 0,
-            source: String::new(),
-            time: String::new(),
-            itin; String::new(),
-            signer_account_id: String::new()
-        }
-    }
 }
 
 impl Default for ClientTestContract {
     fn default() -> Self {
-        // let seed = env::random_seed();
-        // /* Convert to hex string */
-        // let mut value = String::new();
-        // write!(&mut value, "{:x?}", seed).expect("Unable to write");
-        return Self {
-            contract_id: String::from("asddsadsaa"),
-            callbackResponse: Default::default()
-        };
+        env::panic(b"This contract should be initialized before usage")  
     }
 }
 
 #[near_bindgen]
 impl ClientTestContract {
+
+    ///Initialize the contract with a random id
+    #[init]
+    pub fn new()-> Self{
+        /* Prevent re-initializations */
+        assert!(!env::state_exists(), "This contract is already initialized");
+
+         /* Get a random seed to create an unique id for the contract */
+         let seed = &env::random_seed()[..7];
+         /* Convert to hex string */
+         let mut random_id = String::new();
+         for val in seed{
+             write!(&mut random_id, "{:x?}", val).expect("Unable to write")
+         }
+         return Self {
+             contract_id: random_id,
+             callback_response: Response{
+                 err: String::new(),
+                 data: ResponseData::None
+             }
+         };
+    }
+
+
+    /******************/
+    /* Config methods */
+    /******************/
     
     ///Set contract_id to a supplied value
-    pub fn setId(&mut self, contract_id: String)-> String{
+    pub fn set_id(&mut self, contract_id: String)-> String{
         self.contract_id = contract_id;
+        return self.contract_id.clone();
+    }
+
+    ///Get contract_id
+    pub fn get_id(&self)-> String{
         return self.contract_id.clone();
     }
 
@@ -85,7 +108,6 @@ impl ClientTestContract {
     ///Make a request to the dia-gateway smart contract
     #[payable]
     pub fn make_request(&mut self, data_key: String, data_item: String)-> near_sdk::Promise{
-
 
         return near_sdk::Promise::new(String::from(DIA_GATEWAY_ACCOUNT_ID)).function_call(
             b"request".to_vec(),
@@ -100,22 +122,22 @@ impl ClientTestContract {
         );
     }
 
-    ///Callback that the dia-adapter uses
-    pub fn callback(&mut self, symbol: String, name: String, price: f64, price_yesterday: f64, volume_yesterday_USD: f64, source: String, time: String, itin: String, signer_account_id: String){
-        self.callbackResponse.symbol = symbol;
-        self.callbackResponse.name = name;
-        self.callbackResponse.price = price;
-        self.callbackResponse.price_yesterday = price_yesterday;
-        self.callbackResponse.volume_yesterday_USD = volume_yesterday_USD;
-        self.callbackResponse.source = source;
-        self.callbackResponse.time = time;
-        self.callbackResponse.itin = itin;
-        self.callbackResponse.signer_account_id = signer_account_id;
-    }
-
     ///View the dia-adapter response to the contract's request
     pub fn get_callback_response(&self)-> Response{
-        return self.callbackResponse.clone();
+        return self.callback_response.clone();
+    }
+
+
+    /***********************/
+    /* Dia adapter methods */
+    /***********************/
+
+    ///Callback that the dia-adapter uses
+    pub fn callback(&mut self, err: String, response: ResponseData){
+        self.callback_response = Response {
+            err: err,
+            data: response
+        };
     }
 
 }
